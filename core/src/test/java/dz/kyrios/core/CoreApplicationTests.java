9ef0164a6831d3dde4674c8bef49756d35fa52e8
abbes larbaoui <larbaouiabbes@gmail.com>
2025-01-22 19:33:39 +0100

    init core service

---
+++ core/src/test/java/dz/kyrios/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.kyrios.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
